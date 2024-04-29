cff111e21b322ea14ad3e20baa4644f2286129ca
Nolan Moss <nolanmmoss@gmail.com>
2024-04-29 10:38:16 -0400

    add data to excel

--- survery123-webscrape.py
+++ survery123-webscrape.py
@@ -1,10 +1,16 @@
+""""This Python script is used to authenticate and retrieve data from an
+ArcGIS Survey123 survey using the ArcGIS REST API. Let's break it down step by step:"""
+
+# This library is used to send HTTP requests to web servers
 import requests
+
+from openpyxl import Workbook
 
 # Your ArcGIS username and password
 username = "Nolan_HUD"
 password = "nolanm1%HUD"
 
-# URL of the API endpoint to fetch data
+# URL of the API endpoint to fetch data from the Survey123
 api_url = "https://services.arcgis.com/B1yJ6W2oC1kUrC5J/arcgis/rest/services/survey123_bf52e82786ca4d93a5751cc6b0e3833c/FeatureServer/0/query"
 
 # Parameters for the API request
@@ -36,7 +42,7 @@ if login_response.status_code == 200:
     # Add token to API request parameters
     params["token"] = token
 
-    # Send request to fetch data
+    # Send a GET request to fetch data from survey123
     response = requests.get(api_url, params=params)
 
     # Check if request was successful
@@ -47,6 +53,33 @@ if login_response.status_code == 200:
         for feature in data.get("features", []):
             attributes = feature.get("attributes", {})
             print(attributes)
+
+
+
+         # Create a new workbook
+        wb = Workbook()
+        # Get the active worksheet
+        ws = wb.active
+
+        # Add headers to the worksheet
+        data = response.json()
+        headers = data["fields"]
+        header_names = [header["name"] for header in headers]
+        ws.append(header_names)
+
+        # Extract and write data to the worksheet
+        for feature in data.get("features", []):
+            attributes = feature.get("attributes", {})
+            row_data = [attributes.get(header_name, "") for header_name in header_names]
+            
+            ws.append(row_data)
+
+        # Save workbook to Excel file
+        file_path = r"C:\Users\Nolan\Documents\ExcelSheets\Survey123-FuelConsumption.xlsx"
+        wb.save(file_path)
+        print("Data has been written to", file_path)
+
+
     else:
         print("Failed to retrieve data from API. Status code:", response.status_code)
 else:
